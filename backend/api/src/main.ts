ba927e7a6c25153680e3718323b7070333d42158
Oscar Raul Gualdron Villamizar <oscarvilla09@outlook.es>
2025-06-05 09:54:26 -0500

    Cambio de React a nextJs

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -4,6 +4,10 @@ import { config } from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin:'http://localhost:5173'
+  })
   await app.listen(3000);
 }
 bootstrap();
