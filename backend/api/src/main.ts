b3ea5a4128a34a0fd21cb1fa90d5e103ba6ca7f3
Oscar Raul Gualdron Villamizar <oscarvilla09@outlook.es>
2025-06-05 14:30:26 -0500

    Cambios archivo main

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin:'http://localhost:3000',
+    origin:'http://localhost:3001',
     credentials: true
   })
-  await app.listen(3001);
+  await app.listen(process.env.PORT||3001);
 }
 bootstrap();
